0d95b1fda4ca5bbf5312e38616dc26d159f5aeff
brian_li <brian.li@sgs.com>
2024-06-17 13:13:32 +0800

    完成測試列印
    del: remove unnessary code
    new: add comments

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -6,29 +6,13 @@ internal class Program
 {
     static void Main()
     {
-        //Console.WriteLine("Hello, World!");
-        //TSC.About();
+        //TSC.About(); // 顯示 DLL 版本(可測試串 DLL 是否正常)
         PrintLimsLabelFormat1();
     }
 
-    private static void PrintLimsLabelFormat2()
-    {
-        bool IsDualPrint = true;
-        PrintParam pp = SetPrintParam("AVO24600467", "(6/21)");
-
-        TSC.OpenPort("TSC TTP-245");
-        TSC.Setup(73, 15);
-        TSC.ClearBuffer();
-
-        SetContent_01(pp, 10, 10);
-        if (IsDualPrint)
-            SetContent_01(pp, 314, 10);
-
-        TSC.PrintLabel();
-        TSC.ClearBuffer();
-        TSC.ClosePort();
-    }
-
+    /// <summary>
+    /// 列印 LIMS 標籤格式一
+    /// </summary>
     private static void PrintLimsLabelFormat1()
     {
         bool IsDualPrint = true;
@@ -36,12 +20,16 @@ internal class Program
 
         try
         {
+            // 初始化標籤機
             TSC.Build("TSC TTP-245", 73, 15);
 
+            #region 設定列印內容
             SetContent_01(pp, 10, 10);
             if (IsDualPrint)
                 SetContent_01(pp, 314, 10);
+            #endregion
 
+            // 列印標籤
             TSC.Print();
         }
         catch (Exception ex)
@@ -50,10 +38,14 @@ internal class Program
         }
         finally
         {
+            // 釋放資源
             TSC.Dispose();
         }
     }
 
+    /// <summary>
+    /// 設定格式一列印內容
+    /// </summary>
     private static void SetContent_01(PrintParam pp, int offsetX, int offsetY)
     {
         TSC.Qrcode(offsetX, offsetY, pp.Qrcode);
@@ -62,10 +54,16 @@ internal class Program
         TSC.WindowsFont(offsetX, offsetY + 80, pp.RPT_DATE, 24);
     }
 
+    /// <summary>
+    /// 設定列印參數
+    /// </summary>
     private static PrintParam SetPrintParam(string OrdMid, string RptDate)
     {
         return new PrintParam(OrdMid, RptDate, OrdMid, OrdMid);
     }
 
+    /// <summary>
+    /// 精簡版列印參數模型
+    /// </summary>
     private record PrintParam(string ORD_MID, string RPT_DATE, string Qrcode, string Barcode);
 }
